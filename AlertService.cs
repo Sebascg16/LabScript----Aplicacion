2303bdec17264c1d7df65e0a59181b8fb04a9b4f
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-12-04 15:13:03 -0500

    Creación AlertService y Task

---
+++ AlertService.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.JSInterop;
+
+namespace LabScript
+{
+    public class AlertService : IAsyncDisposable
+    {
+        readonly Lazy<Task<IJSObjectReference>> ijsObjectReference;
+        public AlertService(IJSRuntime ijsRuntime)
+        {
+            this.ijsObjectReference = new Lazy<Task<IJSObjectReference>>(() =>
+            ijsRuntime.InvokeAsync<IJSObjectReference>("import", 
+            "./content/LabScript/Pages/Home.razor.js").AsTask());
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+           if (ijsObjectReference.IsValueCreated)
+            {
+                IJSObjectReference moduleJs = await ijsObjectReference.Value;
+                await moduleJs.DisposeAsync();
+            }
+        }
+
+        public async Task CallJsAlertFunction()
+        {
+           var jsModule = await ijsObjectReference.Value;
+
+            await jsModule.InvokeVoidAsync("jsFuncion");
+        }
+    }
+}
